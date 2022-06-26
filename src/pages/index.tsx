c60e4feb6d56c529f72acfba9fc0cb0e81197558
Steven Enriquez <stevenenriquez123@gmail.com>
2022-06-26 10:43:18 -0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,159 @@
+import styles from 'styles/Home.module.scss'
+import ThemeToggleButton from 'components/Theme/ThemeToggleButton'
+import ThemeToggleList from 'components/Theme/ThemeToggleList'
+import { useState } from 'react'
+import { useNetwork, useAccount, useBalance } from 'wagmi'
+import ConnectWallet from 'components/Connect/ConnectWallet'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useSignMessage } from 'wagmi'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div>
+        <ThemeToggleList />
+      </div>
+      <div className="flex items-center">
+        <ThemeToggleButton /> header <ThemeToggleList />
+      </div>
+
+      <div className="flex items-center">
+        <ThemeToggleButton />
+        <ThemeToggleList />
+      </div>
+    </header>
+  )
+}
+
+function Main() {
+  const { data: account } = useAccount()
+  const { activeChain, chains, isLoading: isNetworkLoading, pendingChainId, switchNetwork } = useNetwork()
+  const { data: balance } = useBalance({
+    addressOrName: account?.address,
+  })
+  return (
+    <main className={styles.main + ' space-y-6'}>
+      <div className="text-center">
+        <p className="font-medium">Dapp Starter Boilerplate by arisac.eth</p>
+        <p>
+          <a
+            href="https://github.com/arisac/dapp-starter"
+            target="_blank"
+            className="text-sm underline"
+            rel="noreferrer"
+          >
+            https://github.com/arisac/dapp-starter
+          </a>
+        </p>
+      </div>
+
+      <div>
+        <ConnectWallet />
+      </div>
+
+      <div className="w-full max-w-xl rounded-xl bg-sky-500/10 p-6 text-center">
+        <dl className={styles.dl}>
+          <dt>Connector</dt>
+          <dd>
+            {account && account?.connector?.name}
+            {!account && (
+              <ConnectButton.Custom>
+                {({ openConnectModal }) => (
+                  <span onClick={openConnectModal} className="cursor-pointer hover:underline">
+                    Not connected, connect wallet
+                  </span>
+                )}
+              </ConnectButton.Custom>
+            )}
+          </dd>
+          <dt>Connected Network</dt>
+          <dd>{activeChain ? `${activeChain?.id}: ${activeChain?.name}` : 'n/a'}</dd>
+          <dt>Switch Network</dt>
+          <dd className="flex flex-wrap justify-center">
+            {account &&
+              chains.map(x => (
+                <button
+                  disabled={!switchNetwork || x.id === activeChain?.id}
+                  key={x.id}
+                  onClick={() => switchNetwork?.(x.id)}
+                  className={
+                    (x.id === activeChain?.id ? 'bg-green-500' : 'bg-blue-500 hover:scale-105') +
+                    ' m-1 rounded-lg py-1 px-3 text-white transition-all duration-150'
+                  }
+                >
+                  {x.name}
+                  {isNetworkLoading && pendingChainId === x.id && ' (switching)'}
+                </button>
+              ))}
+            <ConnectWallet show="disconnected" />
+          </dd>
+          <dt>Account</dt>
+          <dd className="break-all">{account ? `${account?.address}` : 'n/a'}</dd>
+          <dt>Balance</dt>
+          <dd className="break-all">{balance ? `${balance?.formatted} ${balance?.symbol}` : 'n/a'} </dd>
+          <dt>Sign Message</dt>
+          <dd className="break-all">{account ? <SignMsg /> : 'n/a'} </dd>
+        </dl>
+      </div>
+    </main>
+  )
+}
+
+function SignMsg() {
+  const [msg, setMsg] = useState('Dapp Starter')
+  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+    message: msg,
+  })
+  const signMsg = () => {
+    if (msg) {
+      signMessage()
+    }
+  }
+
+  return (
+    <>
+      <p>
+        <input value={msg} onChange={e => setMsg(e.target.value)} className="rounded-lg p-1" />
+        <button
+          disabled={isLoading}
+          onClick={() => signMsg()}
+          className="ml-1 rounded-lg bg-blue-500 py-1 px-2 text-white transition-all duration-150 hover:scale-105"
+        >
+          Sign
+        </button>
+      </p>
+      <p>
+        {isSuccess && <span>Signature: {data}</span>}
+        {isError && <span>Error signing message</span>}
+      </p>
+    </>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <ThemeToggleList />
+      </div>
+      <div className="flex items-center">
+        <ThemeToggleButton /> footer <ThemeToggleList />
+      </div>
+
+      <div className="flex items-center">
+        <ThemeToggleButton />
+        <ThemeToggleList />
+      </div>
+    </footer>
+  )
+}
